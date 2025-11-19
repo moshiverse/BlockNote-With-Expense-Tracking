aef260699c802a8c3f474ec37f18fe9c8bb46296
sep <laboradajohnji@gmail.com>
2025-11-19 19:22:58 +0800

    Frontend-Backend Merge

--- BlockNotes/src/main/java/edu/cit/BlockNotes/config/SecurityConfig.java
+++ BlockNotes/src/main/java/edu/cit/BlockNotes/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("https://blocknotes-frontend.onrender.com"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
